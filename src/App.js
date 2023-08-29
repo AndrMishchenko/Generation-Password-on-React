fb29c6639013b1e9818524a22d6a7c29a6f9e5d0
Andrii Mishchenko <vinnypuh1818@gmail.com>
2023-08-29 13:57:54 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
-import {useState} from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Auth from './components/Auth';
+import Home from './components/Home';
 
 function App() {
 
-  const [test, setTest] = useState('test')
-
   return (
     <>
-      <div className='wrapper'>
-        <div className='main-box'>
-          <div className='text'>sdfsdfsd</div>
-          <div className='output'>{test}</div>
-        </div>
-      </div>
+      <Routes>
+        <Route path='/' element={<Auth/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
     </>
   );
 }
